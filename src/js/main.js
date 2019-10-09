0df0645fe3635bd89ae7c11fda59f82da77cf2f2
PavelNestor <pavelnesterets82@gmail.com>
2019-10-10 01:34:28 +0300

    Add main slider

--- src/js/main.js
+++ src/js/main.js
@@ -17,49 +17,43 @@ const $$ = document.querySelectorAll.bind(document);
 //   menuClose.addEventListener("click", onToogleMenu);
 // })();
 
-{
+const handleSliderChange = () => {
   const carousel = $("[data-target='carousel']");
   const leftButton = $("[data-action='slideLeft']");
   const rightButton = $("[data-action='slideRight']");
   const card = carousel.querySelector("[data-target='card']");
+  const cardOffsetWidth = card.offsetWidth;
 
   const carouselWidth = carousel.offsetWidth;
   const cardStyle = card.currentStyle || window.getComputedStyle(card)
   const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
-
-  console.log('carouselWidth', carouselWidth);
-  console.log('cardMarginRight', cardMarginRight);
-  console.log('cardStyle', card.offsetWidth);
-  
-
   const cardCount = carousel.querySelectorAll("[data-target='card']").length;
-  console.log('cardCount', cardCount);
 
   let offset = 0;
-  const maxX = -((cardCount / 3) * carouselWidth + 
-                (cardMarginRight * (cardCount / 3)) - 
-                carouselWidth - cardMarginRight);
-
-  console.log('maxX', maxX);
+  carousel.style.transform = `translateX(${offset}px)`;
+  const maxX = (cardCount - carouselWidth / cardOffsetWidth) * cardOffsetWidth * -1;
 
   const prevSlide = () => {
-  console.log('offset', offset);
-
     if (offset < 0) {
-      offset += card.offsetWidth + cardMarginRight;
-      carousel.style.transform = `translateX(${card.offsetWidth}px)`;
-      }
+      offset += cardOffsetWidth + cardMarginRight;
+      carousel.style.transform = `translateX(${offset}px)`;
+    }
   };
 
   const nextSlide = () => {
-  console.log('offset', offset);
-
     if (offset >= maxX) {
-      offset -= card.offsetWidth + cardMarginRight;
-      carousel.style.transform = `translateX(${-card.offsetWidth}px)`;
+      offset -= cardOffsetWidth + cardMarginRight;
+      carousel.style.transform = `translateX(${offset}px)`;
     }
   };
 
   leftButton.addEventListener("click", prevSlide);
   rightButton.addEventListener("click", nextSlide);
-}
+};
+
+const ready = () => {
+  handleSliderChange();
+};
+
+document.addEventListener('DOMContentLoaded', ready);
+window.addEventListener('resize', handleSliderChange);
