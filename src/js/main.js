631302b03cbd477a5638087fed641395787bd064
PavelNestor <pavelnesterets82@gmail.com>
2019-10-13 02:08:21 +0300

    add main block

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,25 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// (function() {
-//   var menuImg = document.getElementById("menu-image");
-//   var menuClose = document.getElementById("menu-close");
-//   var menuContent = document.getElementById("menu-content");
+(function() {
+  var menuOpen = $('#menu-open');
+  var menuClose = $('#menu-close');
+  var menuContent = $('#menu-mobile');
 
-//   const onToogleMenu = () => {
-//     menuContent.classList.toggle("menu-show");
-//   };
+  const onToogleMenu = () => {
+    menuContent.classList.toggle('menu-mobile_active');
+  };
 
-//   menuImg.addEventListener("click", onToogleMenu);
-//   menuImg.addEventListener("touch", onToogleMenu);
-//   menuClose.addEventListener("click", onToogleMenu);
-// })();
+  menuOpen.addEventListener("click", onToogleMenu);
+  menuClose.addEventListener("click", onToogleMenu);
+})();
 
 const handleSliderChange = () => {
   const carousel = $("[data-target='carousel']");
   const leftButton = $("[data-action='slideLeft']");
   const rightButton = $("[data-action='slideRight']");
   const card = carousel.querySelector("[data-target='card']");
+  const dots = $$('.paginatecircle');
   const cardOffsetWidth = card.offsetWidth;
 
   const carouselWidth = carousel.offsetWidth;
@@ -31,7 +31,19 @@ const handleSliderChange = () => {
 
   let offset = 0;
   carousel.style.transform = `translateX(${offset}px)`;
-  const maxX = (cardCount - carouselWidth / cardOffsetWidth) * cardOffsetWidth * -1;
+  const maxX = (cardCount - Math.floor(carouselWidth / cardOffsetWidth)) * cardOffsetWidth * -1;
+
+
+  dots.forEach(dot => {
+    let slideIndex = dot.dataset.index;
+    
+    dot.addEventListener('click', () => {
+      offset = (cardOffsetWidth + cardMarginRight) * slideIndex * -1;
+      carousel.style.transform = `translateX(${offset}px)`;
+      dots.forEach(dot => dot.classList.remove('paginatecircle_active'));
+      dot.classList.add('paginatecircle_active');
+    });
+  });
 
   const prevSlide = () => {
     if (offset < 0) {
@@ -51,9 +63,26 @@ const handleSliderChange = () => {
   rightButton.addEventListener("click", nextSlide);
 };
 
+//Navbar scrollBehavior
+let prevScrollpos = 0;
+
+const handleScrollBehavior = () => {
+  const navbar = $("#navbar");
+  let currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > 200 && prevScrollpos > currentScrollPos) {
+    navbar.classList.add('navbar_active');
+  } else {
+    navbar.classList.remove('navbar_active');
+  }
+
+  prevScrollpos = currentScrollPos;
+};
+
 const ready = () => {
   handleSliderChange();
 };
 
 document.addEventListener('DOMContentLoaded', ready);
+window.addEventListener('scroll', handleScrollBehavior);
 window.addEventListener('resize', handleSliderChange);
